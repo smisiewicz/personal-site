598105de17d1a6a03b91115c4a461dfe299f81b3
Sebastian Misiewicz <s.p.misiewicz@gmail.com>
2024-10-06 18:17:09 -0400

    bootstrapping

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="landing-page">
+            <div className="text-container">
+                <div className="black-section">
+                    <span className="black-bg">misiewi</span>
+                </div>
+                <div className="white-section">
+                    <span className="white-bg">.cz</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
